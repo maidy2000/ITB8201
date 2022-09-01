eb820572c6dac66bfdc156cc66251954b65fe306
Mattias <mattias.malk@gmail.com>
2022-09-01 20:43:04 +0300

    lmao

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/greet', function (req, res) {
+   res.send("Tere, ".concat(req.query.name))
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
